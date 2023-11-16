dc28b0055d151a2a8c505a62a9191c0a76ce468d
tmdgjs <xfirex1115@naver.com>
2023-11-16 16:14:26 +0900

    init : "initial commit"

---
+++ src/main/java/com/hansheon/shorturl/Application.java
@@ -0,0 +1,16 @@
+package com.hansheon.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
